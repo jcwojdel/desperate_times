dda1690ce47363a79dffedb854cf9928f0842a70
Jacek C Wojdel <j.c.wojdel@gmail.com>
2019-02-28 10:25:45 +0100

    It proves that Cezary G causes problems

--- save_ekskursje.py
+++ save_ekskursje.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 BASE_URL = 'http://wo.blox.pl{}'
-START_URL = '/2019/02/Now-that-it-s-all-over.html'
-
+START_URL_BACKWARD = '/2019/02/Now-that-it-s-all-over.html'
+START_URL_FORWARD = '/2006/08/Currently-playing-4.html'
 
 def get_one_page(url):
     return requests.get(BASE_URL.format(url)).text
@@ -14,14 +14,26 @@ def get_previous_url(content):
     return soup.find(id='BlogWpisPoprzedniLewy').a['href']
 
 
+def get_next_url(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    return soup.find(id='BlogWpisNastepnyPrawy').a['href']
+
+
 def save_page(url, page):
     with open(url.replace('/', '_'), 'w') as f:
        f.write(page)
 
 
-url = START_URL
+url = START_URL_BACKWARD
 while url:
     print(url)
     page = get_one_page(url)
     save_page(url, page)
     url = get_previous_url(page)
+
+url = START_URL_FORWARD
+while url:
+    print(url)
+    page = get_one_page(url)
+    save_page(url, page)
+    url = get_next_url(page)
